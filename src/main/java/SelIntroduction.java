b742c9e4fbb82f39a6dc879d440232cb9f221b71
pado <parmela.dohling@softwareag.com>
2025-04-19 14:19:11 +0530

    Initial commit

---
+++ src/main/java/SelIntroduction.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelIntroduction {
+    public static void main(String[] args){
+//        Invoking browser. Classes-ChromeDriver, Firefox etc -> implements methods of WebDriver interface and have their own methods
+//        ChromeDriver driver = new ChromeDriver(); //This can call methods that are not in WebDriver and present in chromeDriver, which will fail if we want to use Firefox or other browsers
+        //Interface
+        WebDriver driver = new ChromeDriver(); //This line makes sure that only the methods present in the WebDriver interface can be used
+    }
+}
